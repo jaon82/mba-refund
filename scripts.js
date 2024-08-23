9105d91d09f8163f02f5e1ffb0d6e5ee5f4b5c6a
Jean Nunes <jean.nunes@uzzipay.com>
2024-08-22 23:19:45 -0400

    Create object with form fields

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
   value = Number(value) / 100;
@@ -12,3 +16,15 @@ function formatCurrencyBRL(value) {
   });
   return value;
 }
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+};
