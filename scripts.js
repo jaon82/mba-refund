96bc88a032e712ba3e248b134b1cc3f177a39a9b
Jean Nunes <jean.nunes@uzzipay.com>
2024-08-23 00:08:26 -0400

    Add expense

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
+const expenseList = document.querySelector("ul");
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -27,4 +28,36 @@ form.onsubmit = (event) => {
     amount: amount.value,
     created_at: new Date(),
   };
+  addExpense(newExpense);
 };
+
+function addExpense(newExpense) {
+  try {
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", `${newExpense.category_name}`);
+    const expenseInfo = document.createElement("div");
+    expenseInfo.classList.add("expense-info");
+    const expenseName = document.createElement("strong");
+    expenseName.textContent = newExpense.expense;
+    const expenseCategory = document.createElement("span");
+    expenseCategory.textContent = newExpense.category_name;
+    expenseInfo.append(expenseName, expenseCategory);
+    const expenseAmount = document.createElement("span");
+    expenseAmount.classList.add("expense-amount");
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount
+      .toString()
+      .replace("R$", "")}`;
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
+    expenseList.append(expenseItem);
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas");
+    console.log(error);
+  }
+}
